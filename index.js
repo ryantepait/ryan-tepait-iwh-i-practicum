8d8a34a0591990d7c6389a3efb07c818cddb7f7c
Ryan T <ryan@juicetactics.com>
2025-03-31 05:02:33 +0800

    Updated index.js

--- index.js
+++ index.js
@@ -39,54 +39,46 @@ app.get('/', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
+app.get('/update-cobj', (req, res) => {
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
     } catch (error) {
         console.error(error);
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
+
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+
+// * Code for Route 3 goes here
+
+app.post("/update-cobj", async (req, res) => {
+
     const update = {
         properties: {
-            "favorite_book": req.body.newVal
+            name: req.body.name,
+            game: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
         }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
+    const objectURL = `https://api.hubapi.com/crm/v3/objects/games`;
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        "Content-Type": "application/json",
+    };
+
+    try {
+        await axios.post(objectURL, update, { headers });
+        res.redirect("/");
+      } catch (error) {
+        console.error("Error:", error.response?.data || error.message);
+        res.status(500).send('Error creating game object');
+      }
 
 });
-*/
 
 
 // * Localhost
